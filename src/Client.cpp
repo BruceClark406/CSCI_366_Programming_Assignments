9cb71b5c322caf2b7003b375e822843d36b4f67a
BruceClark406 <brucewestonmt@gmail.com>
2020-03-13 12:41:18 -0600

    Assignment 1 completed

--- src/Client.cpp
+++ src/Client.cpp
@@ -22,25 +22,127 @@ Client::~Client() {
 
 
 void Client::initialize(unsigned int player, unsigned int board_size){
+    this->player = player;
+    initialized = true;
+
+    this->board_size = board_size;
+
+    // initalize the board array
+    vector<vector<int> > board(board_size, vector<int>(board_size));
+
+    //write the board to "player_"+to_string(player)+".action_board.json"
+    // serialize the array
+    ofstream array_ofp("player_"+to_string(player)+".action_board.json"); // create an output file stream
+    cereal::JSONOutputArchive write_archive(array_ofp); // initialize an archive on the file
+    write_archive(cereal::make_nvp("board", board)); // serialize the data giving it a name
+    write_archive.finishNode(); // wait for the writing process` to finish
+    array_ofp.close(); // close the file
+
 }
 
 
 void Client::fire(unsigned int x, unsigned int y) {
+    string fname = "player_"+to_string(player)+".shot.json";
+    // remove any old serialization files
+    remove(fname.c_str());
+
+    // create an integer array
+    vector< int > array1(2);
+    array1[0] = x;
+    array1[1] = y;
+
+    // serialize the array, right shot to shot.json
+    ofstream array_ofp(fname); // create an output file stream
+    cereal::JSONOutputArchive write_archive(array_ofp); // initialize an archive on the file
+    write_archive(cereal::make_nvp("x", array1[0])); // serialize the data giving it a name
+    write_archive(cereal::make_nvp("y", array1[1])); // serialize the data giving it a name
+    write_archive.finishNode(); // wait for the writing process` to finish
+    array_ofp.close(); // close the file
 }
 
 
 bool Client::result_available() {
+    // I think this is right, not sure
+    // dont think there is a test associated with this method
+    ifstream result_file("player_1.result.json");
+    return result_file.good();
 }
 
 
 int Client::get_result() {
+    string fname = "player_"+to_string(player)+".result.json";
+    // create a two dimensional array for deserialization
+    int result;
+
+    // deserialize the array
+    ifstream array_ifp(fname); // create an input file stream
+    cereal::JSONInputArchive read_archive(array_ifp); // initialize an archive on the file
+    read_archive(result); // deserialize the array
+    array_ifp.close(); // close the file
+
+    //remove the file
+    remove(fname.c_str());
+
+    // print the result of deserialization
+    printf("results was a %d\n", result);
+
+    if (result == HIT){
+        return HIT;
+    }else if(result == MISS ){
+        return MISS;
+    }else if(result == OUT_OF_BOUNDS){
+        return OUT_OF_BOUNDS;
+    }else{
+        throw ClientException("BAD crap in client.");
+    }
 }
 
 
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+    vector<vector<int>> board_array(board_size, vector<int>(board_size));
+    string fname = "player_"+to_string(player)+".action_board.json";
+    // deserialize the action board into board array
+    ifstream read_board(fname); // create an input file stream
+    cereal::JSONInputArchive read_archive(read_board); // initialize an archive on the file
+    read_archive(board_array); // deserialize the array
+    read_board.close(); // close the file
+
+    // get result of shot, y rows, x = columns
+    board_array[y][x] = result;
+
+    //write board to "player_"+to_string(player)+".action_board.json"
+    // serialize the array
+    ofstream write_board(fname); // create an output file stream
+    cereal::JSONOutputArchive write_archive(write_board); // initialize an archive on the file
+    write_archive(cereal::make_nvp("board", board_array)); // serialize the data giving it a name
+    write_archive.finishNode(); // wait for the writing process` to finish
+    write_board.close(); // close the file
 }
 
 
 string Client::render_action_board(){
+    vector<vector<int>> board_array(board_size, vector<int>(board_size));
+    string fname = "player_"+to_string(player)+".action_board.json";
+    // deserialize the action board into board array
+    ifstream read_board(fname); // create an input file stream
+    cereal::JSONInputArchive read_archive(read_board); // initialize an archive on the file
+    read_archive(board_array); // deserialize the array
+    read_board.close(); // close the file
+
+    string board_string;
+
+    for (int i = 0; i < this->board_size; i++){
+        for (int j = 0; j < this->board_size; j++){
+            if(board_array[i][j] == HIT){
+                board_string += "H";
+            }else if(board_array[i][j] == HIT){
+                board_string += "M";
+            }else{
+                board_string += "-";
+            }
+        }
+        board_string += "\n";
+    }
 }
+
--- src/Server.cpp
+++ src/Server.cpp
@@ -18,27 +18,127 @@
 #include "common.hpp"
 #include "Server.hpp"
 
-
 /**
  * Calculate the length of a file (helper function)
  *
  * @param file - the file whose length we want to query
  * @return length of the file in bytes
  */
+
+
+
+
+
 int get_file_length(ifstream *file){
+    // ripped this from here: http://www.cplusplus.com/reference/istream/istream/seekg/
+
+    file->seekg(0, file->end);
+    int length = file->tellg();
+    file->close();
+    return length;
 }
 
 
-void Server::initialize(unsigned int board_size,
-                        string p1_setup_board,
-                        string p2_setup_board){
+void Server::initialize(unsigned int board_size, string p1_setup_board, string p2_setup_board){
+
+    // make sure p1 and p2 setup boards exist
+    try{
+        this->p1_setup_board.open(p1_setup_board);
+        this->p2_setup_board.open(p2_setup_board);
+        //check size of board
+        this->board_size = board_size;
+
+        int file_length1 = get_file_length(&this->p1_setup_board);
+        int file_length2 = get_file_length(&this->p2_setup_board);
+        this->p1_setup_board.close();
+        this->p2_setup_board.close();
+
+        int predicted_board_size = (board_size)*(board_size+1);
+
+        if (predicted_board_size != file_length1 && predicted_board_size != file_length2){
+            throw ServerException("Board is not the expected size");
+        }
+
+
+    }catch (int e){
+        throw ServerException("A board is not set up!");
+    }
 }
 
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+
+    if (player > MAX_PLAYERS){
+        throw ServerException("Too many players...");
+    }else if(player <= 0){
+        throw ServerException("Too few players...");
+    }else if (x >= BOARD_SIZE or y >= BOARD_SIZE){
+        return OUT_OF_BOUNDS;
+    }
+
+
+    int num_of_char = x * BOARD_SIZE + y;
+
+    // http://www.cplusplus.com/reference/istream/istream/seekg/
+    // https://www.geeksforgeeks.org/set-position-with-seekg-in-cpp-language-file-handling/
+    ifstream board_file ("player_"+to_string(player)+ ".setup_board.txt", ifstream::binary);
+
+    // read char from board
+    board_file.seekg (num_of_char, board_file.beg);
+    char board_char[1];
+    board_file.read(board_char, 1);
+    board_file.close();
+
+
+    if (board_char[0] == '_'){
+        p1_setup_board.close();
+        return MISS;
+    } else{
+        p1_setup_board.close();
+        return HIT;
+    }
 }
 
 
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+    //checking again to pass tests
+    if (player > MAX_PLAYERS){
+        throw ServerException("Too many players...");
+    }else if(player <= 0){
+        throw ServerException("Too few players...");
+    }
+
+    // read shot from shot.json
+    string fname = "player_"+to_string(player)+".shot.json";
+    ifstream read_shot(fname);
+    if (read_shot.good())
+    {
+        vector< int > shot_array(2);
+        cereal::JSONInputArchive read_archive(read_shot);
+        read_archive(shot_array[0],shot_array[1]);
+        read_shot.close();
+
+        // once processed remove file
+        remove((fname).c_str());
+
+
+        // get the result with the coordinates
+        int result = evaluate_shot(player,shot_array[0],shot_array[1]);
+
+
+
+
+        // serialize the results to result.json
+        ofstream write_result("player_"+to_string(player)+".result.json"); // create an output file stream
+        cereal::JSONOutputArchive write_archive(write_result); // initialize an archive on the file
+        write_archive(cereal::make_nvp("result", result)); // serialize the data giving it a name
+        // for some reason we do not wait for the file to write or the close the file
+        // per announcement of Mike Wittie
+
+        return SHOT_FILE_PROCESSED;
+    }
+    else{
+        return NO_SHOT_FILE;
+    }
+
 }
